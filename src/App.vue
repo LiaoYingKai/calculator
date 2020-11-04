392449569d3f7b19e20587f911a13f75a84cabe8
kai <b10223052@yuntech.org.tw>
2020-11-04 17:54:53 +0800

    add component example in app page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calculator">
+    <CalculationBar :formula="['3000', '+', '1000']" :number="4000"/>
+    <CalculationButton :value="1" @onClick="handleClickNumber"> 1 </CalculationButton>
+    <CalculationButton value="/" @onClick="handleClickNumber" type="operator"> / </CalculationButton>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent  } from 'vue'
+import CalculationButton from '@/components/CalculationButton.vue' 
+import CalculationBar from '@/components/CalculationBar.vue' 
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    CalculationButton,
+    CalculationBar
+  },
+  setup() {
+    function handleClickNumber(number) {
+      console.log(number)
+    }
+    return {
+      handleClickNumber
+    }
   }
-}
+})
 </script>
 
 <style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+  background-color: #E8E8E8;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.calculator {
+  width: 350px;
+  height: 525px;
+  background-color: #062145;
+  box-shadow: 0px 20px 40px #00000066;
+  border-radius: 20px;
 }
 </style>
