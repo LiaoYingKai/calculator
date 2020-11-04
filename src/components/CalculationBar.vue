2fb6cd61dfc741c3893c03d9f16cb2044335787a
kai <b10223052@yuntech.org.tw>
2020-11-04 17:54:13 +0800

    add calculation bar component

---
+++ src/components/CalculationBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="calculation-bar">
+    <div class="calculation-bar__formula"> {{ formula.join(' ') }} </div>
+    <div class="calculation-bar__number"> {{formatNumber}} </div>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    formula: {
+      type: Array,
+      default: () => [],
+    },
+    number: {
+      type: [ Number, String ],
+      default: '',
+    }
+  },
+  setup(props) {
+    function format() {
+      // TODO 貨幣格式
+      return props.number
+    }
+
+    const formatNumber = format()
+
+    return {
+      formatNumber
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.calculation-bar {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 8px 16px;
+  height: 109px;
+  text-align: right;
+  white-space:nowrap;
+
+  &__formula {
+    widows: 100%;
+    color: #00C4FF;
+    overflow-x: scroll;
+  }
+  &__number {
+    font-size:56px ;
+    color: #FFFFFF;
+    overflow-x: scroll;
+  }
+}
+
+</style>
