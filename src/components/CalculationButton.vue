75164eaabc502d25a40e31ab8e73215877a87679
kai <b10223052@yuntech.org.tw>
2020-11-04 17:54:32 +0800

    add calculation button component

---
+++ src/components/CalculationButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button @click="handleClick" :class="`${type}-button`">
+    <slot/>
+  </button>
+</template>
+<script>
+import { defineComponent }from 'vue'
+
+export default defineComponent({
+  props: {
+    value: {
+      required: true,
+      type: [String, Number] ,
+    },
+    type: {
+      validator: function (value) {
+        return ['operator', 'default'].includes(value)
+      },
+      default: 'default'
+    }
+  },
+  setup(props, { emit }) {
+    function handleClick() {
+      const { value } = props
+      emit('on-click', value)
+    }
+
+    return {
+      handleClick
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+button {
+  color: #fff;
+  font-size: 24px;
+  box-shadow: 0px 20px 40px #00000066;
+  border-radius: 20px;
+  opacity: 1;
+  border: none;
+  cursor: pointer;
+  width: 72px;
+  height: 64px;
+  padding: 0;
+}
+.operator-button {
+  background-color: #041936;
+}
+.default-button {
+  background-color: transparent;
+}
+</style>
